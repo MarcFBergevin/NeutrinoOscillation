7916efffa75f296a8e0dc56262e886da7e770e22
marcbergevin <mbergevin@ucdavis.edu>
2014-11-12 16:04:18 -0800

    updating the file

--- neutrino.py
+++ neutrino.py
@@ -18,7 +18,7 @@ class NeutrinoOscillation:
     """ Class to display the neutrino oscillation at a specific stand-off """
 
     #
-    # Class Members
+    # Class Members.  Add
     #
     rate        = None
     s12			= 0.0
@@ -33,6 +33,20 @@ class NeutrinoOscillation:
     delta_31 	= 0.0
     delta_21 	= 0.0
     delta_32 	= 0.0
+    
+    #
+    # Define a ROOT object
+    #
+    U235S  = ROOT.TF1("U235S","[0]*exp([1]-[2]*x-[3]*x*x)",1.806,20.00)
+    Pu239S = ROOT.TF1("Pu239S","[0]*exp([1]-[2]*x-[3]*x*x)",1.806,14.000)
+    U238S  = ROOT.TF1("U238S","[0]*exp([1]-[2]*x-[3]*x*x)",1.806,14.000)
+    Pu241S = ROOT.TF1("Pu241S","[0]*exp([1]-[2]*x-[3]*x*x)",1.806,14.000)
+    IBDEnergyTotal = ROOT.TF1
+    
+    nu_U235S   = ROOT.TF1
+    nu_Pu239S  = ROOT.TF1
+    nu_U238S   = ROOT.TF1
+    nu_Pu241S  = ROOT.TF1
 
     #
     # Class methods
@@ -112,7 +126,7 @@ class NeutrinoOscillation:
         elif medium ==0:
             cubicCM    = 1e+6          # m^3 in cm^3
             dens       = 0.804         # g/cm3 from paper
-            mol_weight = 48./352.      #  mols of hydrogen in medium (52./352.not correct for DC)
+            mol_weight = 48./355.      #  mols of hydrogen in medium (52./352.not correct for DC)
         else:
             raise ValueError("You have chosen a non-existing material, Goodbye".format(medium))
         
@@ -120,22 +134,118 @@ class NeutrinoOscillation:
 
         protons    = av_num*mass*cubicCM*dens*mol_weight
         
-        rate   = power * GWth * xsect * protons  * time / length**2/ (4.0*math.pi)
+        self.rate   = power * GWth * xsect * protons  * time / length**2/ (4.0*math.pi)
 
         print 'Detector and reactor characteristics:'
         print ' Detector mass             : %4.3f' %(mass)
         print '          proton-target    : %4.3e' %(protons)
         print ' Reactor  power (GWth)     : %4.3f' %(power)
         print '          distance (km)    : %4.3f' %(standoff)
-        print ' Neutrino rate in detector : %4.3f per day (pre-efficiency)' %(rate)
+        print ' Neutrino rate in detector : %4.3f per day (pre-efficiency)' %(self.rate)
+
+        mNeutron        = 939.565378
+        mProton         = 938.27
+        mElectron       = 0.51099891
+        delta           = mNeutron - mProton
+        A               = 0.5
+        B               = mNeutron*mNeutron
+        C               = 4.0*mProton
+        D               = delta+(delta*delta-mElectron*mElectron)/(2*mProton)
+        E               = mNeutron
+
+        alpha_235U      = 0.496
+        alpha_239P      = 0.351
+        alpha_238U      = 0.087
+        alpha_241P      = 0.066
+
+        beta_235U      = 0.870
+        beta_239P      = 0.896
+        beta_238U      = 0.976
+        beta_241P      = 0.793
+        
+        gamma_235U      = 0.160
+        gamma_239P      = 0.239
+        gamma_238U      = 0.162
+        gamma_241P      = 0.080
+
+        kappa_235U      = 0.091
+        kappa_239P      = 0.0981
+        kappa_238U      = 0.079
+        kappa_241P      = 0.1085
 
 
-    def FindRate(self,medium,mass,power,standoff):
+        Ef_235U         = 201.92
+        Ef_239P         = 209.99
+        Ef_238U         = 205.52
+        Ef_241P         = 213.60
     
+        Ef = alpha_235U*Ef_235U + alpha_239P*Ef_239P + alpha_238U*Ef_238U + alpha_241P*Ef_241P
+        
+        self.U235S.SetParameter(0,alpha_235U)
+        self.U235S.SetParameter(1,beta_235U)
+        self.U235S.SetParameter(2,gamma_235U)
+        self.U235S.SetParameter(3,kappa_235U)
 
+        self.Pu239S.SetParameter(0,alpha_239P)
+        self.Pu239S.SetParameter(1,beta_239P)
+        self.Pu239S.SetParameter(2,gamma_239P)
+        self.Pu239S.SetParameter(3,kappa_239P)
+
+        self.U238S.SetParameter(0,alpha_238U)
+        self.U238S.SetParameter(1,beta_238U)
+        self.U238S.SetParameter(2,gamma_238U)
+        self.U238S.SetParameter(3,kappa_238U)
+
+        self.Pu241S.SetParameter(0,alpha_241P)
+        self.Pu241S.SetParameter(1,beta_241P)
+        self.Pu241S.SetParameter(2,gamma_241P)
+        self.Pu241S.SetParameter(3,kappa_241P)
+
+        IBDXsect = ROOT.TF1("IBDXsect","[0]*(sqrt([1]-[2]*([3]-x))-[4])",1.806,14.000)
+        IBDXsect.SetParameter(0,A)
+        IBDXsect.SetParameter(1,B)
+        IBDXsect.SetParameter(2,C)
+        IBDXsect.SetParameter(3,D)
+        IBDXsect.SetParameter(4,E)
+        Xsect = ROOT.TF1("Xsect","IBDXsect*sqrt(IBDXsect**2-[0]**2)",1.806,14.000)
+                                        
+        self.IBDEnergy = ROOT.TF1("IBDEnergy","(U235S + Pu239S+U238S+Pu241S)*Xsect",1.806,14.000)
+        self.nu_U235S   = ROOT.TF1("nu_U235S",  "(U235S)*Xsect",1.806,14.000)
+        self.nu_Pu239S  = ROOT.TF1("nu_Pu239S","(Pu239S)*Xsect",1.806,14.000)
+        self.nu_U238S   = ROOT.TF1("nu_U238S",  "(U238S)*Xsect",1.806,14.000)
+        self.nu_Pu241S  = ROOT.TF1("nu_Pu241S","(Pu241S)*Xsect",1.806,14.000)
+    
+        self.IBDEnergy.SetNpx(10000)
+        self.nu_U235S.SetNpx(10000)
+        self.nu_Pu239S.SetNpx(10000)
+        self.nu_U238S.SetNpx(10000)
+        self.nu_Pu241S.SetNpx(10000)
+
+        self.IBDEnergy.SetTitle("All isotopes")
+        self.nu_U235S.SetTitle("U235 isotope")
+        self.nu_Pu239S.SetTitle("Pu239 isotope")
+        self.nu_U238S.SetTitle("U238 isotope")
+        self.nu_Pu241S.SetTitle("Pu241 isotope")
+    
+            
+    def ApplyBurnup(self,days):
+        """ Add the burn-up behavior applied """
+        
+        alpha_235U_bu = alpha_235U 
+        alpha_239P_bu = alpha_239P
+        alpha_238U_bu = alpha_238U
+        alpha_241P_bu = alpha_241P
+        
+        self.U235S.SetParameter(0,  alpha_235U_bu)
+        self.Pu239S.SetParameter(0, alpha_239P_bu)
+        self.U238S.SetParameter(0,  alpha_238U_bu)
+        self.Pu241S.SetParameter(0, alpha_241P_bu)
+    
+       
+
+    def FindRate(self):
     
         return self.rate
-
 
 
 
@@ -162,3 +272,16 @@ if __name__ == "__main__":
     nuOsc = NeutrinoOscillation(detectorMedium,detectorMass,reactorPower,reactorStandoff,hierarchy_flag=0)
     # One can change any paramters manually in the following way
     #nuOsc = NeutrinoOscillation(detectorMedium,detectorMass,reactorPower,reactorStandoff,s13=3)
+
+
+
+    nuOsc.nu_U235S.SetLineColor(ROOT.kMagenta)
+    nuOsc.nu_Pu239S.SetLineColor(ROOT.kOrange+8)
+    nuOsc.nu_U238S.SetLineColor(ROOT.kBlue-6)
+    nuOsc.nu_Pu241S.SetLineColor(ROOT.kGreen+3)
+    print '%s ' %(nuOsc.FindRate())
+    nuOsc.IBDEnergy.Draw()
+    nuOsc.nu_U235S.Draw("same")
+    nuOsc.nu_Pu239S.Draw("same")
+    nuOsc.nu_U238S.Draw("same")
+    nuOsc.nu_Pu241S.Draw("same")
